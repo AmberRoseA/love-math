095557c614dbf76ea53ca0bda0b1ae554762114f
AmberRoseA <>
2023-12-13 21:22:33 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("Connected")
+function runGame() {
+
+}
+
+function checkAnswers() {
+
+}
+
+function calculateCorrectAnswers() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
